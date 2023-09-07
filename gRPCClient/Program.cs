559a3ffcee33484a87fc1f6dbcaaa035c77bcbeb
sinancansevinc <sinancan_sevinc@hotmail.com>
2023-09-07 12:46:10 +0300

    Add project files.

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using GrpcDemoService;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7255");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Sino" });
+
+Console.WriteLine($"From server: {reply}");
